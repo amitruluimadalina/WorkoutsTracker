7486492b9ce745d82e465d6b2d6068f0eede0486
amitruluimadalina <104475104+amitruluimadalina@users.noreply.github.com>
2022-05-16 04:33:09 +0300

    Add files via upload

---
+++ Workouts-main/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <WorkoutInput />
+    <WorkoutsList/>
+  </div>
+</template>
+
+<script>
+
+import WorkoutInput from '@/components/WorkoutInput';
+import WorkoutsList from '@/components/WorkoutsList';
+export default {
+  name: 'App',
+  components: {
+  WorkoutInput,
+  WorkoutsList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
